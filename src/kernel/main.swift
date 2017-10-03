5bea304aa782b67ff450da6db2ddde822a4d83ac
quickn <quickn@localhost.localdomain>
2017-10-03 10:15:34 +0900

    base output & start write runtime

--- src/kernel/main.swift
+++ src/kernel/main.swift
@@ -1,4 +1,25 @@
+/*@_silgen_name("outportb") func outportb(_: UInt16, _: UInt8)
+
+public func setCursor(x: UInt16, y: UInt16) {
+	let pos = (y * 80) + x
+	outportb(0x3D4, 0x0F)
+	outportb(0x3D5, UInt8(pos & 0xFF))
+	outportb(0x3D4, 0x0E)
+	outportb(0x3D5, UInt8(pos >> 8 & 0xFF))
+}*/
+
 @_silgen_name("kmain") public func kmain() {
+	let vram = UnsafeMutablePointer<UInt16>(bitPattern: 0xB8000)
+	let vramSize = 80 * 25
+
+	var i = 0
+	repeat {
+		vram?[i] = 0xA00 // clear screen
+		i += 1
+	} while i < vramSize
+
+	vram?[0] = 0x0F48
+
 	while 0 < 1 {
 		// TODO : make print string
 	}
--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -1,17 +1,52 @@
 #define NULL (void *)0
 
-void _TZvOSs7Process5_argcVSs5Int32(void) {
+void _TZvOSs7Process5_argcVSs5Int32(void) 
+{
 	// ...
 }
 
-void _TZvOSs7Process11_unsafeArgvGVSs20UnsafeMutablePointerGS0_VSs4Int8__(void) {
+void _TZvOSs7Process11_unsafeArgvGVSs20UnsafeMutablePointerGS0_VSs4Int8__(void) 
+{
 	// ...
 }
 
-void _TTSf4s_s_d_d___TFs18_fatalErrorMessageFTVs12StaticStringS_S_Su_T_(void) {
+void _TTSf4s_s_d_d___TFs18_fatalErrorMessageFTVs12StaticStringS_S_Su_T_(void) 
+{
 	for (;;);
 }
 
-void swift_once(void *predicate, void (*fn)(void *)) {
+void swift_once(void *predicate, void (*fn)(void *))
+{
 	fn(NULL);
+}
+
+/* Types */
+
+// Int8
+
+void _T0s5UInt8VN(void) 
+{
+	// ..
+}
+
+void _T0s5UInt8Vs17FixedWidthIntegersWP(void)
+{
+	// ..
+}
+
+void _T0s5UInt8Vs15UnsignedIntegersWP(void)
+{
+	// ..
+}
+
+// Int16
+
+void _T0s6UInt16VN(void)
+{
+	// ..
+}
+
+void _T0s6UInt16Vs13BinaryIntegersWP(void)
+{
+	// ..
 }
