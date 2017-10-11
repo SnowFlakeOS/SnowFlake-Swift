70776fde1e526398c3b3154cdf2c971f9bd0efc0
quickn <quickn@localhost.localdomain>
2017-10-11 22:34:55 +0900

    fixed 100% (thanks for https://github.com/charliesome/mini64)

--- src/arch/x86_64/loader.asm
+++ src/arch/x86_64/loader.asm
@@ -13,17 +13,6 @@ _start:
 
     mov rsp, 0xffffff00001ffff8
     xor rbp, rbp
-
-    ; Display "Hello World!"
-    mov edi, 0x00b8000              
- 
-    mov rax, 0x1F6C1F6C1F651F48    
-    mov [edi],rax
- 
-    mov rax, 0x1F6F1F571F201F6F
-    mov [edi + 8], rax
- 
-    mov rax, 0x1F211F641F6C1F72
-    mov [edi + 16], rax
-   ; call kmain     ; Call our kernel's main() function
-     hlt
+    
+    call kmain     ; Call our kernel's main() function
+    hlt
