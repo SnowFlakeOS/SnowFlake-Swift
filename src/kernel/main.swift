7039aae841423a0f09d0efe398d26e10dd6446ae
quickn <quickn@quicknui-MacBook-Pro.local>
2017-11-22 19:49:30 +0900

    Writing Integers.swift (Currently, SnowFlake is will crash

--- src/kernel/main.swift
+++ src/kernel/main.swift
@@ -1,13 +1,3 @@
-/*@_silgen_name("outportb") func outportb(_: UInt16, _: UInt8)
-
-public func setCursor(x: UInt16, y: UInt16) {
-	let pos = (y * 80) + x
-	outportb(0x3D4, 0x0F)
-	outportb(0x3D5, UInt8(pos & 0xFF))
-	outportb(0x3D4, 0x0E)
-	outportb(0x3D5, UInt8(pos >> 8 & 0xFF))
-}*/
-
 @_silgen_name("kmain") public func kmain() {
 	let vram = UnsafeMutablePointer<UInt16>(bitPattern: 0xB8000)
 	let vramSize = 80 * 25
@@ -33,10 +23,10 @@ public func setCursor(x: UInt16, y: UInt16) {
 	let s: StaticString = "Hello, World!"
 
 	i = 0
-	var test = s.utf8Start[i]
+	var test = s.utf8CodeUnitCount
 
-	/*while i < s.utf8CodeUnitCount {
+	while i < s.utf8CodeUnitCount {
 		vram?[i] = UInt16(s.utf8Start[i]) | 0xA00
 		i += 1
-	}*/
+	}
 }
